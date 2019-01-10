4a74729ff3ea69c2aeff06108bc7402092ff22de
charlottecgd <charlottecaignard@gmail.com>
2019-01-10 16:32:09 +0100

    première version html css

---
+++ view/chapitre.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Blog Jean Forteroche</title>
+        <link rel="stylesheet" href="../public/CSS/webfonts.css">
+        <link rel="stylesheet" href="../public/CSS/chapitre.css">
+        <link rel="stylesheet" href="../public/CSS/theme.css">
+        <link rel="stylesheet" href="../public/CSS/header.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    </head>
+ 
+    <body>
+    <header>
+        <div>
+        <h1>Jean Forteroche</h1>
+        <p>Auteur et ecrivain</p>
+        </div>
+        <nav class="menu">
+            <ul>
+                <li><a href="#Accueil">Accueil</a></li>
+                <li><a href="#Roman">Roman</a></li>
+                <li><a href="#Connexion">Connexion</a></li>
+            </ul>
+         </nav>
+    </header>
+    <div class="news">
+        <h4>Chapitre 4</h4>
+        <p>Mensonge, leMensonge, le sophisme, n'est muni d'une grille. Cette brusque apparition avait pétrifié, c'était maintenant seulement qu'il était lancévvMensonge, le sophisme, n'est muni d'une grille. Cette brusque apparition avait pétrifié, c'était maintenant seulement qu'il était lancéMensonge, le sophisme, n'est muni d'une grille. Cette brusque apparition avait pétrifié, c'était maintenant seulement qu'il était lancévvvvvvvvvvvvvvv sophisme, n'est muni d'une grille. Cette brusque apparition avait pétrifié, c'était maintenant seulement qu'il était lancéMensonge, le sophisme, n'est muni d'une grille. Cette brusque apparition avait pétrifié, c'était maintenant seulement qu'il était lancéMensonge, le sophisme, n'est muni d'une grille. Cette brusque apparition avait pétrifié, c'était maintenant seulement qu'il était lancévvvMensonge, le sophisme, n'est muni d'une grille. Cette brusque apparition avait pétrifié, c'était maintenant seulement qu'il était lancé dans une fenêtre. Décidé à partir, et emporter à la nuit. Rends-moi heureux et je serai comme le voyageur, que je pris le chemin de leur pays, moins loin dans certains autres, tant grandes que petites. Empêcher que le souvenir lui revint. Rendez-vous avec elles au champ d'honneur... </p>
+    </div>
+    <img id="img4" src="../public/images/img51.jpg" alt="">
+
+    <div id="formulaire-comment">
+            <h3>Poster un commentaire</h3>
+            <form action="">
+                <input id="input-author-name"type="text" name="author" placeholder="Votre nom ou pseudo">
+                <textarea name="comment" id="" rows="5"  placeholder="Votre commentaire"></textarea>
+                <button type="submit">Envoyer</button>
+            </form>
+    </div>
+    <div id="comments">
+        <h3>Commentaires</h3>
+        <article>
+            <b>Martine</b>
+            <span>Le 11/03/2018</span>
+            <small>
+                <a href="">Signaler</a>
+            </small>
+            <p>Super !</p>
+            <hr>
+        </article>
+        <article>
+            <b>Martine</b>
+            <span>Le 11/03/2018</span>
+            <small>
+                <a href="">Signaler</a>
+            </small>
+            <p>Super !</p>
+            <hr>
+        </article>
+        <article>
+            <b>Martine</b>
+            <span>Le 11/03/2018</span>
+            <small>
+                <a href="">Signaler</a>
+            </small>
+            <p>Super !</p>
+            <hr>
+        </article>
+    </div>
+
+    </div>
+    </body>
+</html>
---
+++ view/roman.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Blog Jean Forteroche</title>
+        <link rel="stylesheet" href="../public/CSS/webfonts.css">
+        <link rel="stylesheet" href="../public/CSS/roman.css">
+        <link rel="stylesheet" href="../public/CSS/theme.css">
+        <link rel="stylesheet" href="../public/CSS/header.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    </head>
+ 
+    <body>
+    <header>
+        <div>
+        <h1>Jean Forteroche</h1>
+        <p>Auteur et ecrivain</p>
+        </div>
+        <nav class="menu">
+            <ul>
+                <li><a href="#Accueil">Accueil</a></li>
+                <li><a href="#Roman">Roman</a></li>
+                <li><a href="#Connexion">Connexion</a></li>
+            </ul>
+         </nav>
+    </header>
+    
+    <div id="last-news">
+        <h3>Derniere publication</h3>
+        <img id="img4" src="../public/images/img4.jpg" alt="">
+        <div>
+        <h4>Chapitre 4</h4>
+        <p>Mensonge, le sophisme, n'est muni d'une grille. Cette brusque apparition avait pétrifié, c'était maintenant seulement qu'il était lancé dans une fenêtre. Décidé à partir, et emporter à la nuit. Rends-moi heureux et je serai comme le voyageur, que je pris le chemin de leur pays, moins loin dans certains autres, tant grandes que petites. Empêcher que le souvenir lui revint. Rendez-vous avec elles au champ d'honneur... </p>
+        <button><i class="fas fa-book-open"></i></button>
+        </div>
+    </div>
+
+    <div class="news">
+        <h4>Chapitre 3</h4>
+        <p>Mensonge, le sophisme, n'est muni d'une grille. Cette brusque apparition avait pétrifié, c'était maintenant seulement qu'il était lancé dans une fenêtre. Décidé à partir, et emporter à la nuit. Rends-moi heureux et je serai comme le voyageur, que je pris le chemin de leur pays, moins loin dans certains autres, tant grandes que petites. Empêcher que le souvenir lui revint. Rendez-vous avec elles au champ d'honneur... </p>
+        <button><i class="fas fa-book-open"></i></button>
+    </div>
+    <div class="news">
+        <h4>Chapitre 2</h4>
+        <p>Mensonge, le sophisme, n'est muni d'une grille. Cette brusque apparition avait pétrifié, c'était maintenant seulement qu'il était lancé dans une fenêtre. Décidé à partir, et emporter à la nuit. Rends-moi heureux et je serai comme le voyageur, que je pris le chemin de leur pays, moins loin dans certains autres, tant grandes que petites. Empêcher que le souvenir lui revint. Rendez-vous avec elles au champ d'honneur... </p>
+        <button><i class="fas fa-book-open"></i></button>
+    </div>
+    <div class="news">
+        <h4>Chapitre 1</h4>
+        <p>Mensonge, le sophisme, n'est muni d'une grille. Cette brusque apparition avait pétrifié, c'était maintenant seulement qu'il était lancé dans une fenêtre. Décidé à partir, et emporter à la nuit. Rends-moi heureux et je serai comme le voyageur, que je pris le chemin de leur pays, moins loin dans certains autres, tant grandes que petites. Empêcher que le souvenir lui revint. Rendez-vous avec elles au champ d'honneur... </p>
+        <button><i class="fas fa-book-open"></i></button>
+    </div>
+    </body>
+</html>
